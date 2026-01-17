5771c44dcc5b7fd04c6c0155aa6d6b74e32f2e57
Taseen Waseq <76471599+taseenw@users.noreply.github.com>
2026-01-17 15:58:37 -0500

    stub-evail-harness v0.1

--- stub-eval-harness/src/eval_runner/cli.py
+++ stub-eval-harness/src/eval_runner/cli.py
@@ -1,15 +1,31 @@
+import json
 import typer
 
 app = typer.Typer(no_args_is_help=True)
 
 @app.command()
-def tester():
-    print("jot")
+def eval_run(
+    config: str = typer.Option(..., "--config", help="Path to config file"),
+):
+    """Run evaluation with the given config file."""
+    print(f"Running eval with config: {config}")
+    # read in file from data/prompts.jsonl its a id, prompt pair
+    with open("data/prompts.jsonl", "r") as f:
+        prompts = [json.loads(line) for line in f]
+    
+    output_dict = {}
+    for prompt in prompts:
+        response = dummyAIPass(prompt)
+        output_dict[prompt["id"]] = response
 
-@app.command()
-def tester2():
-    print("jot2")
+    # write to file in outputs/results.jsonl
+    with open("outputs/results.jsonl", "w") as f:
+        for id, response in output_dict.items():
+            f.write(json.dumps({"id": id, "response": response}) + "\n")
 
-@app.command(name="eval-run")
-def evailrun():
-    print("we in here")
+# Just sends req and return
+def dummyAIPass(prompt_object: dict):
+    # dummy response for id #
+    response = "dummy response for prompt with id: " + prompt_object["id"]
+
+    return response
